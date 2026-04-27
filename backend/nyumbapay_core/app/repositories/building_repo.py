206a103fbd7b83f483e694090d63fda03d77493b
Billytheegoat <billytheegoat@Billytheegoats-MacBook-Pro.local>
2026-04-27 14:00:39 +0300

    database interactions

---
+++ backend/nyumbapay_core/app/repositories/building_repo.py
@@ -0,0 +1,3 @@
+from nyumbapay_core.app.repositories.repos import BuildingRepository
+
+__all__ = ["BuildingRepository"]
---
+++ backend/nyumbapay_core/app/repositories/tenant_repo.py
@@ -0,0 +1,3 @@
+from nyumbapay_core.app.repositories.repos import TenantRepository
+
+__all__ = ["TenantRepository"]
